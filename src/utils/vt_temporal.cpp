9d6d787bccf96f47891cfeb3b097ceb9f2f87909
marcob0020 <marcob0020@gmail.com>
2024-09-22 18:24:24 +0200

    VTDateTime class to abstract temporal representation

---
+++ src/utils/vt_temporal.cpp
@@ -0,0 +1,78 @@
+//
+// Created by marcob0020 on 9/22/24.
+//
+
+#include "vt_temporal.hpp"
+
+namespace utils {
+
+
+    VTDateTimeParameters ParseVTDateTimeParameters(const std::string_view string) {
+        std::pair<DateParameters, LocalTimeParameters> parameters = utils::ParseLocalDateTimeParameters(string);
+
+        return VTDateTimeParameters{parameters.first, parameters.second};
+    }
+
+    VTDateTime::VTDateTime(const int64_t microseconds) {
+        this->microseconds = microseconds;
+    }
+
+    VTDateTime::VTDateTime(const DateParameters &date_parameters, const LocalTimeParameters &local_time_parameters) {
+        utils::LocalDateTime holder(date_parameters, local_time_parameters);
+        this->microseconds = holder.MicrosecondsSinceEpoch();
+    }
+
+    VTDateTime::VTDateTime(const Date &date, const LocalTime &local_time) {
+        utils::LocalDateTime holder(date, local_time);
+        this->microseconds = holder.MicrosecondsSinceEpoch();
+    }
+
+    VTDateTime::VTDateTime(const LocalDateTime &local_date_time) {
+        this->microseconds = local_date_time.MicrosecondsSinceEpoch();
+    }
+
+    VTDateTime::operator LocalDateTime() const {
+        return LocalDateTime(this->microseconds);
+    }
+
+    bool inline VTDateTime::operator>(const VTDateTime &other) const {
+        return this->microseconds > other.microseconds;
+    }
+
+    bool inline VTDateTime::operator<(const VTDateTime &other) const {
+        return this->microseconds < other.microseconds;
+    }
+
+    bool inline VTDateTime::operator==(const VTDateTime &other) const {
+        return this->microseconds == other.microseconds;
+    }
+
+    bool inline VTDateTime::operator!=(const VTDateTime &other) const {
+        return this->microseconds != other.microseconds;
+    }
+
+    bool inline VTDateTime::operator<=(const VTDateTime &other) const {
+        return this->microseconds <= other.microseconds;
+    }
+
+    bool inline VTDateTime::operator>=(const VTDateTime &other) const {
+        return this->microseconds >= other.microseconds;
+    }
+
+    bool inline VTDateTime::operator<=>(const VTDateTime &other) const {
+        return this->microseconds >= other.microseconds;
+    }
+
+    size_t VTDateTimeHash::operator()(const VTDateTime &vt_date_time) const {
+        LocalDateTimeHash hasher;
+
+        return hasher(static_cast<LocalDateTime>(vt_date_time));
+
+    }
+
+    VTDateTime CurrentVTDateTime() {
+        namespace chrono = std::chrono;
+        auto ts = chrono::time_point_cast<chrono::microseconds>(chrono::system_clock::now());
+        return VTDateTime(ts.time_since_epoch().count());
+    }
+} // utils
---
+++ src/utils/vt_temporal.hpp
@@ -0,0 +1,74 @@
+//
+// Copyright 2022 Memgraph Ltd.
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.txt; by using this file, you agree to be bound by the terms of the Business Source
+// License, and you may not use this file except in compliance with the Business Source License.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0, included in the file
+// licenses/APL.txt.
+//
+// Created by marcob0020 on 9/22/24.
+//
+
+#pragma once
+
+#include "temporal.hpp"
+
+namespace utils {
+
+struct VTDateTimeParameters {
+  DateParameters date;
+  LocalTimeParameters time;
+
+  bool operator==(const VTDateTimeParameters &) const = default;
+};
+
+VTDateTimeParameters ParseVTDateTimeParameters(std::string_view string);
+
+class VTDateTime {
+private:
+    int64_t microseconds;
+public:
+    explicit VTDateTime(int64_t microseconds);
+    explicit VTDateTime(const DateParameters &date_parameters, const LocalTimeParameters &local_time_parameters);
+    explicit VTDateTime(const Date &date, const LocalTime &local_time);
+    explicit VTDateTime(const LocalDateTime &local_date_time);
+
+    explicit operator LocalDateTime() const;
+    bool inline operator> (const VTDateTime &other) const;
+    bool inline operator < (const VTDateTime &other) const;
+    bool inline operator == (const VTDateTime &other) const;
+    bool inline operator != (const VTDateTime &other) const;
+    bool inline operator <= (const VTDateTime &other) const;
+    bool inline operator >= (const VTDateTime &other) const;
+    bool inline operator <=> (const VTDateTime &other) const;
+
+    friend std::ostream &operator<<(std::ostream &os, const VTDateTime &ldt) {
+        os << LocalDateTime(ldt.microseconds);
+        return os;
+    }
+
+    friend VTDateTime operator+(const VTDateTime &dt, const Duration &dur) {
+        return VTDateTime(dt.microseconds + dur.microseconds);
+    }
+
+    friend VTDateTime operator+(const Duration &dur, const VTDateTime &dt) { return dt + dur; }
+
+    friend VTDateTime operator-(const VTDateTime &dt, const Duration &dur) { return dt + (-dur); }
+
+    friend Duration operator-(const VTDateTime &lhs, const VTDateTime &rhs) {
+        return Duration(lhs.microseconds) - Duration(rhs.microseconds);
+    }
+};
+
+struct VTDateTimeHash {
+    size_t operator()(const VTDateTime &vt_date_time) const;
+};
+
+VTDateTime CurrentVTDateTime();
+
+} // utils
+
